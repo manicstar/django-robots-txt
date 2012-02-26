89b0331ce5f1893c70c9a52329c3715f85656f25
Nicolas Kuttler <office@nicolaskuttler.com>
2012-02-26 19:22:56 +0100

    Update template location
    Well... use an app-specific directory for the robots.txt template, just
    to be safe.

--- robots_txt/views.py
+++ robots_txt/views.py
@@ -5,7 +5,7 @@ class RobotsTextView(TemplateView):
     """
     A simple view with mimetype text/plain
     """
-    template_name = 'robots.txt'
+    template_name = 'robots_txt/robots.txt'
 
     def render_to_response(self, context, **kwargs):
         return super(RobotsTextView, self).render_to_response(
